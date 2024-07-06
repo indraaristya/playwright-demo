2c4b70459f40aaa2dc0f196a4e9037cd0328da18
Indra Aristya <indra.aristya@mekari.com>
2024-07-06 11:13:38 +0800

    add new sample tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,8 +4,9 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -14,8 +15,8 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  retries: process.env.RETRY ? parseInt(process.env.RETRY) : 0,
+  workers: process.env.WORKER ? parseInt(process.env.WORKER) : 1,
   reporter: [
     ['html', { open: 'never' }],
     ['list'],
@@ -27,7 +28,7 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
+      name: 'Chrome',
       use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     }
   ],
